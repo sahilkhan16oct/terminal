06aa3c98aaeb1d9a8971b15c95f61a678b8c143d
sahilkhan16oct <slkn121316@gmail.com>
2025-09-26 15:43:29 +0530

    update build

--- interview-platform/src/config.js
+++ interview-platform/src/config.js
@@ -1,4 +1,4 @@
-const API_BASE_URL="localhost:4000";
-// const API_BASE_URL="18.233.164.192:4000";
+// const API_BASE_URL="localhost:4000";
+const API_BASE_URL="52.207.248.207:4000";
 
 export default API_BASE_URL;
